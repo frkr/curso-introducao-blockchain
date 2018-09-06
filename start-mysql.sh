60739142c6e7b738d44acfc5c90a8a243f58ac89
Davi <davimesquita@gmail.com>
2018-09-06 16:03:39 -0300

    MYSQL_ALLOW_EMPTY_PASSWORD

--- start-mysql.sh
+++ start-mysql.sh
@@ -1,4 +1,4 @@
-echo docker run --name mysql-blockchain -e MYSQL_USER=test -e MYSQL_PASSWORD=123456 -e MYSQL_ROOT_PASSWORD=123456 -d mysql
+echo docker run --name mysql-blockchain -e MYSQL_USER=test -e MYSQL_PASSWORD=123456 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_ROOT_PASSWORD=123456 -d mysql
 echo --name mysql-blockchain
 
 sudo docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql:5.5
